3316fc64dd7c262f1769e5b4d9afc8578c267ccb
Bogdan Klimov <136115919+BogdanKlimov11@users.noreply.github.com>
2024-03-19 16:18:37 +0300

    Create Timsort.py

---
+++ Sorting_algorithms/Timsort.py
@@ -0,0 +1,55 @@
+def insertion_sort(arr, left=0, right=None):
+    if right is None:
+        right = len(arr) - 1
+
+    for i in range(left + 1, right + 1):
+        key = arr[i]
+        j = i - 1
+        while j >= left and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+def merge(arr, left, mid, right):
+    len_left, len_right = mid - left + 1, right - mid
+    left_arr, right_arr = arr[left:mid + 1], arr[mid + 1:right + 1]
+
+    i = j = 0
+    k = left
+
+    while i < len_left and j < len_right:
+        if left_arr[i] <= right_arr[j]:
+            arr[k] = left_arr[i]
+            i += 1
+        else:
+            arr[k] = right_arr[j]
+            j += 1
+        k += 1
+
+    while i < len_left:
+        arr[k] = left_arr[i]
+        i += 1
+        k += 1
+
+    while j < len_right:
+        arr[k] = right_arr[j]
+        j += 1
+        k += 1
+
+def timsort(arr):
+    min_run = 32
+    n = len(arr)
+
+    for i in range(0, n, min_run):
+        insertion_sort(arr, i, min((i + min_run - 1), n - 1))
+
+    size = min_run
+    while size < n:
+        for start in range(0, n, 2 * size):
+            mid = min(n - 1, start + size - 1)
+            end = min((start + 2 * size - 1), (n - 1))
+            merge(arr, start, mid, end)
+        size *= 2
+
+    return arr
